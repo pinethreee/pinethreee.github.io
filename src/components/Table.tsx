21a075da0a70f89072c66ab241fc01d318a5c6b0
green <greenn.lab@gmail.com>
2025-02-19 00:04:41 +0900

    add

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,15 +2,17 @@ import {ReactNode} from 'react';
 
 type TableOptions = {
   readonly width?: number[],
+  readonly headers?: string[],
   readonly children: ReactNode[]
 };
 
-export default function Table({width, children}: TableOptions) {
+export default function Table({width, headers, children}: TableOptions) {
   return (
       <table className={'pe--table'}>
         <colgroup>
-          {(width || [20, 60, 20]).map((w, i) => <col key={i} width={w}/>)}
+          {(width || [20, 20, 60]).map((w, i) => <col key={i} width={w}/>)}
         </colgroup>
+        {headers && <thead> {headers.map((h, j) => <th key={j}>{h}</th>)} </thead>}
         {children}
       </table>
   );
